5361a70e34210b1bda4662bcdbf62bc871c3fb17
Akamate Chayapiwat <aukmate@gmail.com>
2017-12-10 22:23:33 +0700

    2

--- MyGame/src/logic/Item.java
+++ MyGame/src/logic/Item.java
@@ -3,6 +3,7 @@ package logic;
 import java.util.Random;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import sharedObject.IRenderable;
 import sharedObject.RenderableHolder;
 import window.SceneManager;
@@ -11,23 +12,38 @@ public class Item extends CollidableEntity{
 	private int x;
 	private int y;
 	public int itemType;
+	public int itemTick=0;
 	public Item() {
 		Random rand= new Random();
 		this.x=rand.nextInt(SceneManager.SCENE_WIDTH);
 		this.y=rand.nextInt(SceneManager.SCENE_HEIGHT);
 		randomItem();
+		this.setImage(new Image("res/Armor.png"));
 	}
 	private void randomItem() {
 		Random rand= new Random();
 		this.itemType=rand.nextInt(2);
 	}
 	public void update() {
-		System.out.println(2);
+		this.itemTick++;
+		if(this.itemTick>=600) { 
+			  this.destroyed=true;
+			  this.itemTick=0;
+			}
+		else {
+			for (IRenderable i : RenderableHolder.getInstance().getEntities()) {
+				if (this.collideWith((CollidableEntity) i)) {
+					if (i instanceof Tank)
+						this.destroyed = true;
+						this.itemTick=0;
+				}
+			}
+		}
 	}
 	@Override
 	public void draw(GraphicsContext gc) {
 		// TODO Auto-generated method stub
-		gc.fillRect(x, y, 20, 20);
+		gc.drawImage(this.image, x, y);
 	}
 
 }
